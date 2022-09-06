9d0e563b5682bb8231ec5ff35de1429d5b4baaca
paige-clark <paigeclarkillustration@gmail.com>
2022-09-06 12:49:48 -0400

    Add conditional text to DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,28 @@ export default function DayListItem(props) {
     'day-list__item--full': !props.spots,
   });
 
+  // this can be implemented, however I was able to solve this with && conditionals
+  const formatSpots = function (spotsRemaining) {
+    if (spotsRemaining > 1) {
+      return `${spotsRemaining} spots remaining`;
+    }
+    if (spotsRemaining === 1) {
+      return `1 spot remaining`;
+    }
+    if (!spotsRemaining) {
+      return `no spots remaining`;
+    }
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">
+        {/* {formatSpots(props.spots)} */}
+        {props.spots > 1 && <>{props.spots} spots remaining</>}
+        {props.spots === 1 && <>1 spot remaining</>}
+        {!props.spots && <>no spots remaining</>}
+      </h3>
     </li>
   );
 }
