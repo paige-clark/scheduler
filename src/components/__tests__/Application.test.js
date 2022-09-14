67a98fd5384ecf4d0c1027b2572aaad88cfbbd00
paige-clark <paigeclarkillustration@gmail.com>
2022-09-13 20:58:51 -0400

    Add application tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,6 +6,10 @@ import {
   fireEvent,
   prettyDOM,
   getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+  queryByText,
 } from '@testing-library/react';
 import Application from 'components/Application';
 
@@ -41,11 +45,34 @@ describe('Application', () => {
   });
 
   // TEST 03
-  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
-  
-    await waitForElement(() => getByText(container, "Archie Cohen"));
-  
-    console.log(prettyDOM(container));
+  it('loads data, books an interview and reduces the spots remaining for Monday by 1', async () => {
+    const { container, debug } = render(<Application />);
+    // get info after app loads
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointments = getAllByTestId(container, 'appointment');
+    const appointment = appointments[0];
+
+    // 4 x events needed to trigger appointment save
+    fireEvent.click(getByAltText(appointment, 'Add'));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: 'Lydia Miller-Jones' },
+    });
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+    fireEvent.click(getByText(appointment, 'Save'));
+
+    // while site saves, expect saving to be on screen
+    expect(getByText(appointment, 'Saving...')).toBeInTheDocument();
+
+    // wait for Lydia to be on screen
+    await waitForElement(() => getByText(appointment, 'Lydia Miller-Jones'));
+
+    // confirm that no spots remaining shows when monday is full
+    const day = getAllByTestId(container, 'day').find((day) =>
+      queryByText(day, 'Monday')
+    );
+    expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
+    // console.log(prettyDOM(day));
+    // debug();
   });
 });
